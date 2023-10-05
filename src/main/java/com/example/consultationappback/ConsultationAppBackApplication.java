8a7ae8c9ef47b71e9d4c91099626c7874dc49208
rovshan <rouvsen@gmail.com>
2023-10-05 06:07:23 +0400

    Update configs

---
+++ src/main/java/com/example/consultationappback/ConsultationAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultationappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultationAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consultationappback/ConsultationAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultationappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
